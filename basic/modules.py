803a95bce527e1506f8f78b6ef827d37cea6fe72
alejopua <alejopua@gmail.com>
2024-05-12 17:29:27 -0500

    feat: Add modules.py file with date and math functions
    This commit adds the `modules.py` file, which includes imports for the `date` class from the `datetime` module and the `sum` and `subtract` functions from the `fmath` module. It also demonstrates the usage of these functions by performing addition and subtraction operations on numbers and printing the current date.

---
+++ basic/modules.py
@@ -0,0 +1,16 @@
+# own modules
+# thirdy party modules
+# python modules
+
+
+from datetime import date
+from fmath import sum, subtract
+
+today = date.today()
+print(today)
+
+sum1 = sum(1, 2)
+print(sum1)
+
+res1 = subtract(10, 2)
+print(res1)
