be6a2f53ee2a65dc097e3017e3e792d15c5ac908
alejopua <alejopua@gmail.com>
2024-05-12 17:28:56 -0500

    Add dictionaries.py file with product data and print methods

---
+++ basic/dictionaries.py
@@ -0,0 +1,22 @@
+products = [
+  {
+    "name": "laptop",
+    "price": 800,
+  },
+  {
+    "name": "mouse",
+    "price": 40,
+  },
+  {
+    "name": "monitor",
+    "price": 100,
+  }
+]
+
+product =   {
+    "name": "laptop",
+    "price": 800,
+}
+
+print(product.keys())
+print(product.values())
