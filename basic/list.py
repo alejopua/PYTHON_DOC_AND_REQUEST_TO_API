36dad75e415921a41a6570cebbf9408b71e0ea0d
alejopua <alejopua@gmail.com>
2024-05-12 17:29:16 -0500

    feat: Add sorting functionality to list.py

---
+++ basic/list.py
@@ -0,0 +1,43 @@
+# list are mutable allow to change the values after their creation 
+
+# otherList = [1, 'hello', 3.14 , True , [1, 2, 3]]
+# print(otherList)
+
+# things = ['a', 'b', 'c', 'd']
+# print(dir(things))
+# print(things)
+
+# numberList = list((1, 2, 3, 4, 5))
+# print(numberList)
+# print(1 in numberList )
+
+# listOfRange = list(range(1, 10))
+# print(listOfRange)
+
+colors = ['red', 'blue', 'green', 'yellow']
+print(colors)
+# colors[1] = 'black'
+# print(colors)
+# colors.append('purple')
+# print(colors)
+# colors.extend(['white', 'pink'])
+
+# colors.insert(0, 'orange')
+# print(colors)
+
+# colors.remove('green')
+# print(colors)
+
+# colors.pop()
+# print(colors)
+
+# colors.clear()
+# print(colors)
+
+colors.sort()
+print(colors)
+
+colors.sort(reverse=True)
+print(colors)
+
+
