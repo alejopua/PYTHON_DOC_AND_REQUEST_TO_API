36cd52a94596076e46eb2fe79895cf6bf208ffbb
alejopua <alejopua@gmail.com>
2024-05-12 17:29:47 -0500

    Add variables.py file with variable declarations and assignments

---
+++ basic/variables.py
@@ -0,0 +1,15 @@
+# VARIABLES are case sensitive
+# Variables can be declared again and again
+name = "Peter"
+name = "Ryan"
+print(name)
+
+x, y = 100, 200
+print(x, y)
+
+house_name = "White House" # snake case
+houseName = "White House" # camel case
+HouseName = "White House" # pascal case
+
+# mean that the variable is constant
+PI = 3.1416
