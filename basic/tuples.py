243050b6503fd0636b20c1061ba90ce12010cffe
alejopua <alejopua@gmail.com>
2024-05-12 17:29:44 -0500

    Add tuples.py file with tuples examples

---
+++ basic/tuples.py
@@ -0,0 +1,9 @@
+# tuples are inmutable don't allow to change the values after their creation
+
+months = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+print(months)
+
+locations = {
+  (35.6895, 139.6917): 'Tokyo',
+  (40.7128, 74.0060): 'New York'
+}
