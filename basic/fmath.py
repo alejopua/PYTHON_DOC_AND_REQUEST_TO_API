3f3460b7cc0180a1c567e4c9a803d0827ba23a1b
alejopua <alejopua@gmail.com>
2024-05-12 17:29:05 -0500

    feat: Add basic math functions to fmath.py
    This commit adds the `sum` and `subtract` functions to the `fmath.py` file. These functions perform addition and subtraction operations on two numbers respectively. The functions also include input validation to ensure that both numbers are provided before carrying out the operation.

---
+++ basic/fmath.py
@@ -0,0 +1,11 @@
+def sum(x, y):
+  if x == None or y == None:
+    return "Please provide two numbers for carry out operation"
+  else:
+    return x + y
+
+def subtract(x, y):
+  if x == None or y == None:
+    return "Please provide two numbers for carry out operation "
+  else:
+    return x - y
