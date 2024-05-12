c9abc56edefe41fe7fa239fa2dfaa9c681195572
alejopua <alejopua@gmail.com>
2024-05-12 17:29:40 -0500

    Add basic string operations to strings.py

---
+++ basic/strings.py
@@ -0,0 +1,31 @@
+# myName = "peter ramirez"
+
+# print(myName.upper())
+# print(myName.capitalize())
+# print(myName.replace("peter", "alejandro"))
+# print(myName.count("e"))
+# print(myName.split(" "))
+# print(myName.find("r"))
+# print(len(myName))
+
+# print(myName.index("r"))
+
+# print(myName.isalpha())
+# print(myName.isalpha())
+
+# print(myName[4])
+# print(myName[-1])
+
+# templete strings
+name = "Peter"
+age = 25
+print("Hello, my name is {} and I am {} years old".format(name, age))
+print(f"Hello, my name is {name} and I am {age} years old")
+
+
+
+
+
+
+
+
