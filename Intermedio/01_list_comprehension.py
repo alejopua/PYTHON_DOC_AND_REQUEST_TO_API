cc2db089176bdbfd9ab2435566413532ce0b9b74
alejopua <alejopua@gmail.com>
2024-05-12 17:29:56 -0500

    feat: Add list comprehension example
    This commit adds an example of list comprehension to the `01_list_comprehension.py` file. It demonstrates how to create a new list by multiplying each element of the original list by 2. The original list and the new list are printed for verification.

---
+++ Intermedio/01_list_comprehension.py
@@ -0,0 +1,6 @@
+### list comprehension ###
+my_original_list = [0, 1, 2, 3, 4, 5, 6, 7]  
+my_list = [i*2 for i in range(8)]
+
+print(my_original_list)  # [1, 2, 3, 4, 5]
+print(my_list)  # [0, 1, 2, 3, 4, 5, 6, 7]
