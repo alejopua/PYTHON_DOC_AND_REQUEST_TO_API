2411ed817e562dfae640c13e6f7a751bfdba3e0f
alejopua <alejopua@gmail.com>
2024-05-12 17:29:12 -0500

    feat: Add introduction to Python and basic data types

---
+++ basic/hello_world.py
@@ -0,0 +1,47 @@
+# INTRODUCE TO PYTHON
+# Language general purpose
+# Language hight level 
+# type dynamic
+# easy to learn
+# PYTHON SHINE LIKE A APP SCIENCES
+
+# Strings
+print("hello world")
+print('hello world')
+print('''hello world''')
+print("""hello world""")
+
+# type of data
+print(type("hello world"))
+
+# Concatenation
+print("Bye " + " World")
+
+# Numbers
+print(30) # Integer
+print(30.5) # Float
+
+# Boolean
+True
+False
+
+# List can change
+[10, 20, 30, 55]
+["hello", "bye", "adios"]
+[10, "hello", True, 10.1]
+
+# Tuples can't change
+(10, 20, 30, 55)
+
+# Dictionaries
+{
+    "name": "Ryan",
+    "lastname": "Ray",
+    "nickname": "Fazt"
+}
+
+
+
+
+
+
