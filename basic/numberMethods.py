4b0b40d5f9054efdc28a43e2c90d97bf304ac8d4
alejopua <alejopua@gmail.com>
2024-05-12 17:29:32 -0500

    feat: Add numberMethods.py file with number operations
    This commit adds the `numberMethods.py` file, which includes code for performing number operations. It demonstrates the order of precedence for mathematical operations and calculates the result of an arithmetic expression. It also prompts the user to enter their age and displays it along with the age incremented by 5.

---
+++ basic/numberMethods.py
@@ -0,0 +1,13 @@
+# Numbers operations
+# order the precedence / * + -
+
+a = 10
+b = 20
+c = 30
+d = 40
+
+print(a + b * c / d)
+
+age = input("Enter your age: ")
+print(f"Your age is: {age}")
+print(f"New age with 5 more is: {age} + 5 = {int(age) + 5}")
